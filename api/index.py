6106a4ddf3978c65000e1d4861f2ace95d1b8938
Kristoffer Prestvold <kristoffer.prestvold@vegvesen.no>
2026-04-12 11:09:57 +0200

    Simplify /validate endpoint to test API routing

--- api/index.py
+++ api/index.py
@@ -27,22 +27,4 @@ async def validate(
     ifc_file: UploadFile = File(...),
     ids_file: UploadFile = File(...),
 ):
-    if not ifc_file.filename.endswith(".ifc"):
-        raise HTTPException(400, "Filen må være en .ifc-fil")
-    if not ids_file.filename.endswith(".ids"):
-        raise HTTPException(400, "Filen må være en .ids-fil")
-
-    with tempfile.NamedTemporaryFile(suffix=".ifc", delete=False) as tmp_ifc:
-        tmp_ifc.write(await ifc_file.read())
-        ifc_path = tmp_ifc.name
-
-    with tempfile.NamedTemporaryFile(suffix=".ids", delete=False) as tmp_ids:
-        tmp_ids.write(await ids_file.read())
-        ids_path = tmp_ids.name
-
-    try:
-        result = run_ids_check(ifc_path, ids_path)
-        return result
-    finally:
-        os.unlink(ifc_path)
-        os.unlink(ids_path)
+    return {"test": "API is working", "files": [ifc_file.filename, ids_file.filename]}
